c69ab3a1220c1e994d354b4ff721a8b280db0c35
lamdb182779 <lamdb183779@gmail.com>
2025-05-26 23:56:03 +0700

    fix service status

--- src/services/dto/update-service.dto.ts
+++ src/services/dto/update-service.dto.ts
@@ -1,6 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateServiceDto } from './create-service.dto';
+import { IsIn, IsOptional } from 'class-validator';
 
 export class UpdateServiceDto extends PartialType(CreateServiceDto) {
+    @IsOptional()
+    @IsIn(["Chờ xác nhận", "Chấp thuận", "Từ chối", "Đã hủy"])
     status: "Chờ xác nhận" | "Chấp thuận" | "Từ chối" | "Đã hủy";
 }
